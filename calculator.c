1a255e40764deb3882246b83d671dc1ba75f86f4
shubhamji25122007-maker <shubhamji25122007@gmail.com>
2026-02-15 21:53:30 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,b,c;
+    printf("enter the first number ");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&b);
+    c=a+b;
+
+    printf(" your sum is %d",c);
+    return 0;
+
+}
